51866f6861457b2b768a09aa2d2d68df5cde4e6d
the1900 <the1900.me@gmail.com>
2020-05-20 14:03:08 +0900

    막힘ㅋ

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 #[cfg(windows)] extern crate winapi;
 use std::io::Error;
 
-fn print_message_gui(msg: &str) -> Result<i32, Error> {
-    use std::ffi::OsStr;
-    use std::iter::once;
-    use std::os::windows::ffi::OsStrExt;
+extern "C" fn callback(handle: winapi::shared::basetsd::HANDLE_PTR, params: winapi::shared::minwindef::LPARAM) {
+    println!("I'm called from C with value {0}", handle);
+}
+
+fn listing_windows() -> Result<i32, Error> {
+    // use std::ffi::OsStr;
+    // use std::iter::once;
+    // use std::os::windows::ffi::OsStrExt;
     use std::ptr::null_mut;
-    use winapi::um::winuser::{MB_OK, MessageBoxW};
-    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+    use winapi::um::winuser::EnumDesktopWindows;
+    // let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
     let ret = unsafe {
-        MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK)
+        EnumDesktopWindows(null_mut(), callback, 0)
     };
     if ret == 0 { Err(Error::last_os_error()) }
     else { Ok(ret) }
 }
+
+/*
+https://docs.microsoft.com/ko-kr/windows/win32/api/winuser/nf-winuser-enumdesktopwindows?redirectedfrom=MSDN 써서 현재 데스크톱에서 실행중인 프로그램 목록 얻어오고
+Rust 에서 NULL을 넘기는법
+RUST 에서 함수 포인터를 넘기는법 몰겠네
+*/
 
 fn main() {
     // if OS is not Windows then terminate the program.
@@ -22,5 +32,5 @@ fn main() {
         std::process::exit(0x01);
     }
 
-    print_message_gui("yeah").unwrap();
+    listing_windows().unwrap();
 }
