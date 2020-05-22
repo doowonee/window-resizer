eec3731f4eead63ff2dc7ca675090d8e7f55179f
the1900 <the1900.me@gmail.com>
2020-05-22 17:31:01 +0900

    feat: add filter hwnd from EnumDesktopWindows
    It is not perfect. It's still shows me other windows.
    For example Microsoft Text Input Application,  Microsoft Store.

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ use winapi::shared::minwindef::LPARAM;
 // use winapi::um::winuser::WNDENUMPROC;
 use winapi::shared::minwindef::BOOL;
 use std::ptr::null_mut;
-use winapi::um::winuser::{EnumDesktopWindows, GetWindowTextA, GetForegroundWindow};
+use winapi::um::winuser::{EnumDesktopWindows, GetWindowTextA, GetForegroundWindow, IsWindowVisible};
 
 unsafe extern "system" fn callback(handle: HWND, _params: LPARAM) -> BOOL {
     let name = Vec::with_capacity(1024); 
     let ptr = name.as_ptr();
     // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowtexta
     let return_value = GetWindowTextA(handle, ptr as *mut i8, 1024);
-    println!("#@ callback from EnumDesktopWindows: {:?}-{:?}:{:?}", handle, CStr::from_ptr(ptr), return_value);
+    // https://docs.microsoft.com/ko-kr/windows/win32/api/winuser/nf-winuser-iswindowvisible?redirectedfrom=MSDN
+    if return_value > 0 && IsWindowVisible(handle) != 0 {
+        // filter windows through has window's title bar and is visible
+        println!("#@ callback from EnumDesktopWindows: {:?}-{:?}", handle, CStr::from_ptr(ptr));
+    }
     1 as BOOL
 }
 
@@ -45,6 +49,6 @@ fn main() {
         std::process::exit(0x01);
     }
 
-    unsafe { show_currnet_window_info(); }
-    // listing_windows().unwrap();
+    // unsafe { show_currnet_window_info(); }
+    listing_windows().unwrap();
 }
