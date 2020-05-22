66ede0fdee8209fcfac7a96bbcd2a729c5efc7d0
the1900 <the1900.me@gmail.com>
2020-05-22 15:40:42 +0900

    오 어느정도 성공
    WNDENUMPROC 타입 형식으로 콜백 함수 넘기는거 했다. 근데 현재 실행중인 GUI 프로그램 뿐만 아니라 이상한것도 가저오는거 같은데;

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
-#[cfg(windows)] extern crate winapi;
 use std::io::Error;
+use std::ffi::CStr;
+use winapi::shared::windef::HWND;
+use winapi::shared::minwindef::LPARAM;
+// use winapi::um::winuser::WNDENUMPROC;
+use winapi::shared::minwindef::BOOL;
+use std::ptr::null_mut;
+use winapi::um::winuser::{EnumDesktopWindows, GetWindowTextA};
 
-extern "C" fn callback(handle: winapi::shared::basetsd::HANDLE_PTR, params: winapi::shared::minwindef::LPARAM) {
-    println!("I'm called from C with value {0}", handle);
+unsafe extern "system" fn callback(handle: HWND, _params: LPARAM) -> BOOL {
+    let name = Vec::with_capacity(1024); 
+    let ptr = name.as_ptr();
+    // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowtexta
+    let return_value = GetWindowTextA(handle, ptr as *mut i8, 1024);
+    println!("#@ callback from EnumDesktopWindows: {:?}-{:?}:{:?}", handle, CStr::from_ptr(ptr), return_value);
+    1 as BOOL
 }
 
 fn listing_windows() -> Result<i32, Error> {
     // use std::ffi::OsStr;
     // use std::iter::once;
     // use std::os::windows::ffi::OsStrExt;
-    use std::ptr::null_mut;
-    use winapi::um::winuser::EnumDesktopWindows;
     // let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
     let ret = unsafe {
-        EnumDesktopWindows(null_mut(), callback, 0)
+        // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-enumdesktopwindows
+        EnumDesktopWindows(null_mut(), Some(callback), 0 as LPARAM)
     };
     if ret == 0 { Err(Error::last_os_error()) }
     else { Ok(ret) }
 }
-
-/*
-https://docs.microsoft.com/ko-kr/windows/win32/api/winuser/nf-winuser-enumdesktopwindows?redirectedfrom=MSDN 써서 현재 데스크톱에서 실행중인 프로그램 목록 얻어오고
-Rust 에서 NULL을 넘기는법
-RUST 에서 함수 포인터를 넘기는법 몰겠네
-*/
 
 fn main() {
     // if OS is not Windows then terminate the program.
